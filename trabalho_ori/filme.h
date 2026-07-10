cb581efd6cb76714622b37c2b6c3d0a11b713aba
LudmylaSchiavinatt <ludmyla.schiavinatto@estudante.ufscar.br>
2026-07-10 14:50:52 -0300

    adicionando led...

--- trabalho_ori/filme.h
+++ trabalho_ori/filme.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-// Estrutura com exatamente 212 bytes
+// Estrutura de tamanho fixo com campo para encadeamento da LED
 typedef struct {
     int id;
     char titulo[100];
@@ -13,7 +13,8 @@ typedef struct {
     int duracao;
     char classificacao[5];
     float nota;
-    char removido; // '0' para ativo, '1' para removido
+    char removido;       // '0' para ativo, '1' para removido
+    long prox_livre;     // Guarda o offset do próximo espaço vazio na LED (-1 se fim da lista)
 } Filme;
 
 // Protótipos das funções de manipulação de arquivo
@@ -24,5 +25,8 @@ void inserir_filme(FILE* arq, Filme f);
 Filme buscar_filme(FILE* arq, int id);
 int atualizar_filme(FILE* arq, int id, Filme novos_dados);
 int remover_filme(FILE* arq, int id);
+
+// Funções utilitárias para a LED exigidas pelo módulo
+long contar_espacos_livres(FILE *arq);
 
 #endif
